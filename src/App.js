40efc758745cd3407c7407a051b5d4281c4858e1
Zelda <lumakawar@hotmail.com>
2022-07-27 15:52:39 -0700

    dotslash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './src/App.css';
-import Home from "./src/components/Home"
-import Contact from "./src/components/Contact"
-import Footer from "./src/components/Footer"
-import Header from "./src/components/Header"
-import Portfolio from "./src/components/Portfolio"
-import Resume from "./src/components/Resume"
+import './App.css';
+import Home from "./components/Home"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Portfolio from "./components/Portfolio"
+import Resume from "./components/Resume"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
   return (
@@ -12,11 +12,12 @@ function App() {
       <BrowserRouter>
       <Header/>
       <Routes>
+<Route path="/" element={<Home/>}/>
 <Route path="/Home" element={<Home/>}/>
 <Route path="/Contact" element={<Contact/>}/>
 <Route path="/Portfolio" element={<Portfolio/>}/>
 <Route path="/Resume" element={<Resume/>}/>
-<Route path="/" element={<Home/>}/>
+
 
 
 
