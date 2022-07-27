d6e065e1fa05598117a9c9d8f11e135bf1c7186a
Zelda <lumakawar@hotmail.com>
2022-07-27 16:36:40 -0700

    footerfix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -71,10 +71,7 @@ onBlur={handleChange}
 <button type="submit">Submit</button>
 </form>
 
-<a href="https://github.com/linklg1"> <img id="github" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github icon"/> </a>
-<a href="https://www.linkedin.com/in/luma-kawar-b215281b9/"> <img id="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/f/f8/LinkedIn_icon_circle.svg" alt="linkedin icon"/> </a>
-<a href="https://open.spotify.com/user/lumaaaa29"> <img id="spotify" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20191006032433" alt="spotify icon"/> </a>
-<a href="https://lumaness.tumblr.com/"> <img id="tumblr" src="https://upload.wikimedia.org/wikipedia/commons/4/43/Tumblr.svg" alt="tumblr icon"/> </a>
+
 
 </section>
 
